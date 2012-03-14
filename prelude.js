32d55427dc568bbac975c3ec201bdf162c3ebf63
Michael Williamson <mike@zwobble.org>
2012-03-14 21:30:38 +0000

    Refactor $shed to simplify layout

--- prelude.js
+++ prelude.js
@@ -1,33 +1,36 @@
 global.$shed = global.$shed || {};
+$shed.modules = $shed.modules || {};
 
 var print = function(value) {
     process.stdout.write(value);
 };
 
-var $number = function(value) {
-    return {
-        $value: value,
-        equals: function(other) {
-            return value === other.$value;
-        },
-        subtract: function(other) {
-            return $number(value - other.$value);
-        },
-        add: function(other) {
-            return $number(value + other.$value);
-        },
-        toString: function() {
-            return value.toString();
-        }
+(function() {
+    var number = $shed.number = function(value) {
+        return {
+            $value: value,
+            equals: function(other) {
+                return value === other.$value;
+            },
+            subtract: function(other) {
+                return number(value - other.$value);
+            },
+            add: function(other) {
+                return number(value + other.$value);
+            },
+            toString: function() {
+                return value.toString();
+            }
+        };
     };
-};
-    
-var $exportModule = function(name, value) {
-    var parts = name.split(".");
-    var current = $shed;
-    for (var i = 0; i < parts.length - 1; i += 1) {
-        current[parts[i]] = current[parts[i]] || {};
-        current = current[parts[i]];
-    }
-    current[parts[parts.length - 1]] = value;
-};
+        
+    $shed.exportModule = function(name, value) {
+        var parts = name.split(".");
+        var current = $shed;
+        for (var i = 0; i < parts.length - 1; i += 1) {
+            current[parts[i]] = current[parts[i]] || {};
+            current = current[parts[i]];
+        }
+        current[parts[parts.length - 1]] = value;
+    };
+})();
