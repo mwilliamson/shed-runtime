c2206556f8a8c73508f9e41f4cbe0ffb23025978
Michael Williamson <mike@zwobble.org>
2012-04-13 19:45:01 +0100

    Split out regex.js from prelude.js

---
+++ js/regex.js
@@ -0,0 +1,32 @@
+$shed.exportModule("regex", function() {
+    var options = $shed.js.import("options");
+    return {
+        create: function(shedRegexString) {
+            var RegexResult = function(jsResult) {
+                return {
+                    $isShedType: true,
+                    capture: function(index) {
+                        return $shed.string(jsResult[index.$value]);
+                    }
+                };
+            };
+            RegexResult.$isShedType = true;
+            
+            var regex = new RegExp(shedRegexString.$value);
+            return {
+                $isShedType: $shed.boolean(true),
+                test: function(shedString) {
+                    return $shed.boolean(regex.test(shedString.$value));
+                },
+                exec: function(shedString) {
+                    var result = regex.exec(shedString.$value);
+                    if (result === null) {
+                        return options.none;
+                    } else {
+                        return options.some(RegexResult)(RegexResult(result));
+                    };
+                }
+            };
+        }
+    };
+});
