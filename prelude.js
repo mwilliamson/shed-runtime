49a8128c32d2647ab34980f7cc4b2c94b2c061df
Michael Williamson <mike@zwobble.org>
2012-05-10 14:34:27 +0100

    Split out bootstrapping code for modules into bootstrap.js

--- prelude.js
+++ prelude.js
@@ -1,14 +1,8 @@
-global.$shed = global.$shed || {};
-$shed.modules = $shed.modules || {};
-
 var dummyType = {
     $isShedType: true
 };
 
 (function() {
-    var modules = {
-    };
-    
     $shed.unit = {};
     
     var number = $shed.number = function(value) {
@@ -62,60 +56,6 @@ var dummyType = {
     
     var boolean = $shed.boolean = function(value) {
         return value;
-    };
-        
-    $shed.exportModule = function(name, func) {
-        var evaluate = function() {
-            var value = func();
-            modules[name].value = value;
-            modules[name].evaluated = true;
-            var parts = name.split(".");
-            var current = $shed.modules;
-            for (var i = 0; i < parts.length - 1; i += 1) {
-                current[parts[i]] = current[parts[i]] || {};
-                current = current[parts[i]];
-            }
-            current[parts[parts.length - 1]] = value;
-        };
-        modules[name] = {
-            evaluate: evaluate,
-            evaluated: false,
-            value: null
-        };
-    };
-
-    $shed.import = function(name) {
-        var identifiers = name.$value.split(".");
-        var moduleResult = findParentModule(identifiers);
-        if (!moduleResult) {
-            throw new Error("Could not find module: " + name.$value);
-        }
-        var module = moduleResult.module;
-        if (!module.evaluated) {
-            module.evaluate();
-        }
-        
-        var value = module.value;
-        for (var depth = moduleResult.depth; depth < identifiers.length; depth += 1) {
-            if (!Object.prototype.hasOwnProperty.call(value, identifiers[depth])) {
-                throw new Error("Could not find module: " + name.$value);
-            }
-            value = value[identifiers[depth]];
-        }
-        return value;
-    };
-    
-    var findParentModule = function(identifiers) {
-        for (var depth = identifiers.length; depth >= 1; depth -= 1) {
-            var module = modules[identifiers.slice(0, depth).join(".")];
-            if (module) {
-                return {
-                    module: module,
-                    depth: depth
-                };
-            }
-        }
-        return null;
     };
     
     var ImmutableArrayList = function(values) {
@@ -181,12 +121,6 @@ var dummyType = {
             return function(array) {
                 return ImmutableArrayList(array);
             };
-        }
-    };
-    
-    $shed.js = {
-        import: function(name) {
-            return $shed.import($shed.string(name));
         }
     };
 })();
