6305a6f78fcd5af9fa7cadcf52f4d2ffe7bb506d
Michael Williamson <mike@zwobble.org>
2012-05-17 23:42:21 +0100

    Add strings.joinSequence

--- js/_strings.js
+++ js/_strings.js
@@ -1,8 +1,8 @@
 $shed.exportModule("_strings", function() {
     var lists = $shed.js.import("lists");
     return {
-        join: function(separator, sequenceable) {
-            var jsString = lists.sequenceToList($shed.string)(sequenceable.toSequence())
+        joinSequence: function(separator, sequence) {
+            var jsString = lists.sequenceToList($shed.string)(sequence)
                 .$toJsArray()
                 .map(function(shedString) {
                     return shedString.$value;
