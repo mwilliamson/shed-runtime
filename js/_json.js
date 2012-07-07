37d0e6a01cd7f031df5beae9fee8f6028b5df665
Michael Williamson <mike@zwobble.org>
2012-07-07 20:12:57 +0100

    Add json.stringifyString

--- js/_json.js
+++ js/_json.js
@@ -2,6 +2,9 @@ $shed.exportModule("_json", function() {
     return {
         parseString: function(shedString) {
             return $shed.string(JSON.parse(shedString.$value));
+        },
+        stringifyString: function(shedString) {
+            return $shed.string(JSON.stringify(shedString.$value));
         }
     };
 });
