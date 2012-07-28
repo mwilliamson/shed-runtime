b503860cc310f928a25cb595eb1633198fc4ab0c
Michael Williamson <mike@zwobble.org>
2012-07-28 17:31:16 +0100

    Move Tuple constructor into tuples.js

--- js/tuples.js
+++ js/tuples.js
@@ -1,10 +1,60 @@
 $shed.exportModule("tuples", function() {
+    function $tupleConstructor(values) {
+        this.$values = values;
+    }
+
+    $tupleConstructor.prototype.$class = Tuple;
+
+    $tupleConstructor.prototype.equals = function(other) {
+        if (classOf(other) !== Tuple) {
+            return false;
+        }
+        if (this.$values.length !== other.$values.length) {
+            return false;
+        }
+        for (var i = 0; i < this.$values.length; i += 1) {
+            if (!equal(this.$values[i], other.$values[i])) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    $tupleConstructor.prototype.represent = function() {
+        var valuesString = this.$values.map(function(value) {
+            return represent(value).$value;
+        }).join(", ");
+        return $shed.string(
+            "tuple(".concat(valuesString).concat(")")
+        );
+    };
+
+    $tupleConstructor.prototype.append = function(value) {
+        var newValues = this.$values.slice(0);
+        newValues.push(value);
+        return tuple.apply(this, newValues);
+    };
+
+    $tupleConstructor.prototype.appendDestructive = function(value) {
+        this.$values.push(value);
+        return this;
+    };
+
+    $tupleConstructor.prototype.map = function(func) {
+        return func.apply(null, this.$values);
+    };
+
+    var Tuple = $shed.class(function(values) {
+        return new $tupleConstructor(values);
+    }, "Tuple");
+
     return {
         head: function(tuple) {
             return tuple.$values[0];
         },
         $createFromArray: function(array) {
             return tuple.apply(this, array);
-        }
+        },
+        Tuple: Tuple
     };
 });
--- prelude.js
+++ prelude.js
@@ -326,59 +326,16 @@ var List = function() {
     return dummyType;
 };
 
-function $tupleConstructor(values) {
-    this.$values = values;
-}
-
-$tupleConstructor.prototype.$class = Tuple;
-
-$tupleConstructor.prototype.equals = function(other) {
-    if (classOf(other) !== Tuple) {
-        return false;
+var tuple = (function() {
+    var Tuple = $shed.js.import("tuples.Tuple");
+    
+    function tuple() {
+        var values = Array.prototype.slice.call(arguments, 0);
+        return Tuple(values);
     }
-    if (this.$values.length !== other.$values.length) {
-        return false;
-    }
-    for (var i = 0; i < this.$values.length; i += 1) {
-        if (!equal(this.$values[i], other.$values[i])) {
-            return false;
-        }
-    }
-    return true;
-};
-
-$tupleConstructor.prototype.represent = function() {
-    var valuesString = this.$values.map(function(value) {
-        return represent(value).$value;
-    }).join(", ");
-    return $shed.string(
-        "tuple(".concat(valuesString).concat(")")
-    );
-};
-
-$tupleConstructor.prototype.append = function(value) {
-    var newValues = this.$values.slice(0);
-    newValues.push(value);
-    return tuple.apply(this, newValues);
-};
-
-$tupleConstructor.prototype.appendDestructive = function(value) {
-    this.$values.push(value);
-    return this;
-};
-
-$tupleConstructor.prototype.map = function(func) {
-    return func.apply(null, this.$values);
-};
-
-var Tuple = $shed.class(function(values) {
-    return new $tupleConstructor(values);
-}, "Tuple");
-
-function tuple() {
-    var values = Array.prototype.slice.call(arguments, 0);
-    return Tuple(values);
-}
+    
+    return tuple;
+})();
 
 var tupleFromSequence = function(sequence) {
     var values = [];
